1fa73e97c3a6ba7252334e75847bc0c068223068
artamonovoleg <artamonovav2003@gmail.com>
2021-06-26 22:45:00 +0300

    Fix names/headers

--- build/submodules/glslang/External/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artamonovoleg/Documents/VulkanAbstractionLayer/submodules/glslang/External
-# Build directory: /home/artamonovoleg/Documents/VulkanAbstractionLayer/build/submodules/glslang/External
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/submodules/glslang/glslang/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artamonovoleg/Documents/VulkanAbstractionLayer/submodules/glslang/glslang
-# Build directory: /home/artamonovoleg/Documents/VulkanAbstractionLayer/build/submodules/glslang/glslang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("OSDependent/Unix")
